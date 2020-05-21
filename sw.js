892bfa50fa912ce95563b8ff68cbb8facd19e4e6
cade.gardner <d00299297@dmail.dixie.edu>
2020-05-21 12:01:37 -0600

    favicon

--- sw.js
+++ sw.js
@@ -9,36 +9,13 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll([
-        './pics/add_picture_button.svg',
-        './pics/button_down.png',
-        './pics/button_up.png',
-        './pics/date_button.svg',
-        './pics/down_direction.svg',
-        './pics/name_button.svg',
-        './pics/notes_button.svg',
-        './pics/send_button.svg',
-        './pics/trail_button.svg',
-        './pics/trail_conditions_button.svg',
-        './pics/trail_status_button.svg',
-        './pics/up_direction.svg',
-        './pics/visitation_button.svg',
-        './pics/weather_button.svg',
-        './pics/zion_vem_buttons-04.svg',
-        './pics/zion_vem_buttons-05.svg',
-        './android-chrome-192x192.png',
-        './android-chrome-512x512.png',
-        './apple-touch-icon.png',
-        './browserconfig.xml',
-        './favicon-16x16.png',
-        './favicon-32x32.png',
-        './favicon.ico',
-        './mstile-150x150.png',
-        './safari-pinned-tab.svg',
+        './icons',
+        './fonts',
         './site.webmanifest',
         './sw.js',
-        './trailTracker.css',
+        './style.css',
         './index.html',
-        './trailTracker.js'
+        './index.js'
 
       ]).then(() => self.skipWaiting());
     })
@@ -55,19 +32,3 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
-
-// var cacheName = 'hello-pwa';
-// var filesToCache = [
-//   '/',
-//   '/index.html',
-//   '/css/style.css',
-//   '/js/main.js'
-// ];
-
-
-
-// /* Serve cached content when offline */
-// self.addEventListener('fetch', function(e) {
-//   e.respondWith(
-//     caches.match(e.request).then(function(response) {
-//       return response || fetch(e.request);
