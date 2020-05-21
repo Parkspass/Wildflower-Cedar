11198ae28172e8f19ec5f1dce0427eb0b83cb0be
treydin6 <treydinw@gmail.com>
2020-05-21 14:47:51 -0600

    added tab bar and highlighted images for each tab

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ var app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: {
-        page: 'landing' // loading, landing
+        page: 'landing', // loading, landing
+        hambugerMenuIcon: 'icons/hamburger_menuDark.svg',
+        wildflowerIcon: 'icons/flowerDark.svg',
+        treesIcon: 'icons/treeDark.svg'
+    },
+    methods: {
+        setAppBarIcon: function(tab){ 
+            this.resetAppBarTabs();
+            console.log('tab: ', tab);
+            switch(tab){
+                case 'hamburger': this.hambugerMenuIcon = 'icons/hamburger_menuLight.svg'; break;
+                case 'flower': this.wildflowerIcon = 'icons/appbar_flower.svg'; break;
+                case 'tree': this.treesIcon = 'icons/treeLight.svg'; break;
+            }
+        },
+        resetAppBarTabs: function() {
+            this.hambugerMenuIcon = 'icons/hamburger_menuDark.svg';
+            this.wildflowerIcon = 'icons/flowerDark.svg';
+            this.treesIcon = 'icons/treeDark.svg';
+        }
+        
     },
     created: function(){
 
     },
-    methods: function(){
-
-    },
+    
 });
