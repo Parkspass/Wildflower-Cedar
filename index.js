f72e31e4963a26464762771d033cd36c255acf7c
cade.gardner <d00299297@dmail.dixie.edu>
2020-05-21 10:08:03 -0600

    2nd commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+var app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+
+    },
+    created: function(){
+
+    },
+    methods: function(){
+
+    },
+});
