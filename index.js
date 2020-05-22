541623ae0da45139f3b15d7403ca079b684a2186
ckgard27 <ckgard27@gmail.com>
2020-05-22 12:49:51 -0600

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: {
-        page: 'research', // loading, landing, research
+        page: 'home', // loading, landing, research
         hambugerMenuIcon: 'icons/hamburger_menuDark.svg',
         wildflowerIcon: 'icons/flowerDark.svg',
         treesIcon: 'icons/treeDark.svg',
