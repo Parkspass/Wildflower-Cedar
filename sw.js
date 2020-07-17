f34e734b23285c5b59aed2408bcd69c580ad2a6f
cade.gardner <d00299297@dmail.dixie.edu>
2020-07-17 16:48:44 -0600

    new

--- sw.js
+++ sw.js
@@ -1,34 +1,56 @@
- /*jshint esversion: 6 */
+/*jshint esversion: 6 */
 
-const cacheName = 'v1::static';
+const staticCacheName = 'site-static-v1'; // Need to change version number ANYTIME we change HTML or CSS files. This is how we get the app to update. The browser needs to detect a change in the service worker.
+const assets = [
+    '/', // caches all reqeust responses from server
+    '/index.html',
+    '/app.js',
+    '/style.css',
+    '/favicon.ico',
+    '/icons',
+    '/fonts/Libre_Baskerville/LibreBaskerville-Regular.ttf',
+    '/fonts/Libre_Baskerville/LibreBaskerville-Italic.ttf',
+    '/fonts/Open_Sans/OpenSans-Regular.ttf',
+];
 
-
-self.addEventListener('install', function(e) {
-  // once the SW is installed, go ahead and fetch the resources
-  // to make this work offline
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll([
-        './icons',
-        './fonts',
-        './site.webmanifest',
-        './sw.js',
-        './style.css',
-        './index.html',
-        './index.js'
-
-      ]).then(() => self.skipWaiting());
-    })
-  );
+// Install service worker
+self.addEventListener('install', evt => {
+    // This will cache everything we need to work offline
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log("Caching assets...");
+            cache.addAll(assets);
+        })
+    );
 });
 
-// when the browser fetches a url, either response with
-// the cached object or go ahead and fetch the actual url
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    // ensure we check the *right* cache to match against
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+// Acivate event
+self.addEventListener('activate', evt => {
+    // This function will delete all the older caches so the app stays up to date
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
 });
+
+// Fetch Event
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request); // Returns chache response. If response is not there, it will try to return the event request
+        })
+    );
+    //console.log("Fetch event", evt);
+});
+
+/* EXTRA PWA NOTES
+- This PWA does not require dynamic caching (strategically only caching pages that the user will use offline)
+If you want to do dynamic caching watch video #18 by The Net Ninja on Youtube
+- This app also only has one HTML page. If you want a pretty fallback page for the user to see if it can't get the cached page, watch video #19.
+- You can also limit cache sizes if you have extensive apps
+
+*/
