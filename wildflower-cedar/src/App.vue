c30600377995f66582f1f6e6f58d5f8e37c4f6eb
Reidemption <75095757+Reidemption@users.noreply.github.com>
2022-04-18 18:05:57 -0600

    outline of home page

--- wildflower-cedar/src/App.vue
+++ wildflower-cedar/src/App.vue
@@ -23,50 +23,47 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,600;0,700;1,600&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Fraunces:wght@300;400;600&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,600;0,700;1,600&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Fraunces:wght@300;400;600&display=swap");
 h1 {
-  font-family: 'Fraunces', serif;
+  font-family: "Fraunces", serif;
   font-weight: 400;
   font-size: 3em;
 }
 h2 {
-  font-family: 'Fraunces', serif;
+  font-family: "Fraunces", serif;
   font-weight: 300;
   font-size: 2em;
-
 }
 h3 {
-  font-family: 'Fraunces', sans-serif;
+  font-family: "Fraunces", sans-serif;
   font-weight: 600;
   font-size: 1.125em;
 }
 .link {
-  font-family: 'Fraunces', serif;
+  font-family: "Fraunces", serif;
   font-weight: 600;
   font-size: 1em;
+  color: #8ea396;
 }
 .subtitle {
-  font-family: 'Fraunces', serif;
+  font-family: "Fraunces", serif;
   font-weight: 400;
   font-size: 1em;
 }
 .primary-button {
-  font-family: 'Sans Source Pro', serif;
+  font-family: "Sans Source Pro", serif;
   font-weight: 700;
   font-size: 1.125em;
 }
 .search {
-  font-family: 'Sans Source Pro', sans-serif;
+  font-family: "Sans Source Pro", sans-serif;
   font-weight: 600;
-  font-size: 1.0em;
+  font-size: 1em;
 }
 .navigation {
-  font-family: 'Sans Source Pro', sans-serif;
+  font-family: "Sans Source Pro", sans-serif;
   font-weight: 600;
   font-size: 0.875em;
 }
-
-
-
 </style>
--- wildflower-cedar/src/components/Home.vue
+++ wildflower-cedar/src/components/Home.vue
@@ -11,136 +11,39 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h2>
-          Cedar Breaks
-        </h2>
-        <h1>
-          Wildflowers
-        </h1>
+        <h2>Cedar Breaks</h2>
+        <h1>Wildflowers</h1>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-          <v-btn
-            class="primary-button"
-            href="/search"
-            color="sageGreen"
-          >
-            Search
-          </v-btn>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
+      <v-col class="mb-5" cols="12">
+        <v-btn class="primary-button" href="/color" color="sageGreen" rounded>
+          Step by Step Verification
+        </v-btn>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
+      <v-col class="mb-5" cols="12">
+        <v-btn
+          light
+          color="lightSage white--text"
+          href="/search"
+          rounded
+          class="primary-button"
+        >
+          Search by Name
+        </v-btn>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+      <v-col class="mb-5" cols="12">
+        <p class="link" color="sageGreen--text">
+          <u> Wildflower Festival - July 2022 </u>
+        </p>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-
 export default {
-  name: 'HelloWorld',
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
-}
+  name: "Home",
+};
 </script>
